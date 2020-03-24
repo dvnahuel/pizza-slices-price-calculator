0f2560d839cdea842d3001be349e7eb233882634
Nahuel Del Valle <nahuel@Nahuels-MacBook-Pro.local>
2020-03-24 14:54:55 -0300

    New console changes

--- main.js
+++ main.js
@@ -17,6 +17,16 @@ function calculate(){
     }
 }
 
+function consoleLog() {
+    const cssRule =
+          "color: rgb(249, 162, 34);" +
+          "font-size: 60px;" +
+          "font-weight: bold;" +
+          "text-shadow: 1px 1px 5px rgb(249, 162, 34);" +
+          "filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
+        console.log("%cHey there 🤙🏻", cssRule);
+  }
 
+  consoleLog();
 
 
